df1cb16e9a826d016b073a4213354b8b7a6ee904
Chris Nixon <chris.d.nixon@gmail.com>
2019-02-21 14:50:06 -0800

    ApplicationScope request element is removed from FindCustomersOrCustomersInfo in v12

--- lib/bing-ads-api/service/customer_management.rb
+++ lib/bing-ads-api/service/customer_management.rb
@@ -1,13 +1,13 @@
 # -*- encoding : utf-8 -*-
 module BingAdsApi
-	
-	
-	# Public : This class represents the Customer Management Services 
-	# defined in the Bing Ads API, to manage customer accounts 
-	# 
-	# Author:: jlopezn@neonline.cl 
-	# 
-	# Examples 
+
+
+	# Public : This class represents the Customer Management Services
+	# defined in the Bing Ads API, to manage customer accounts
+	#
+	# Author:: jlopezn@neonline.cl
+	#
+	# Examples
 	#  options = {
 	#    :environment => :sandbox,
 	#    :username => "username",
@@ -17,15 +17,15 @@ module BingAdsApi
 	#    :account_id => "9876543" }
 	#  service = BingAdsApi::CustomerManagement.new(options)
 	class CustomerManagement < BingAdsApi::Service
-		
-		
-		# Public : Constructor 
-		# 
-		# Author:: jlopezn@neonline.cl 
-		# 
+
+
+		# Public : Constructor
+		#
+		# Author:: jlopezn@neonline.cl
+		#
 		# options - Hash with the parameters for the client proxy and the environment
-		# 
-		# Examples 
+		#
+		# Examples
 		#   options = {
 		#     :environment => :sandbox,
 		#     :username => "username",
@@ -44,26 +44,26 @@ module BingAdsApi
 		## Operations Wrappers ##
 		#########################
 
-		# Public : Gets a list of objects that contains account identification information, 
+		# Public : Gets a list of objects that contains account identification information,
 		# for example the name and identifier of the account, for the specified customer.
-		# 
-		# Author:: jlopezn@neonline.cl 
-		# 
+		#
+		# Author:: jlopezn@neonline.cl
+		#
 		# === Parameters
 		# +customer_id+ - identifier for the customer who owns the accounts. If nil, then the authentication customer id is used
-		# +only_parent_accounts+ - boolean to determine whether to return only the accounts that belong to the customer or to also 
-		# return the accounts that the customer manages for other customers. Default false 
-		# 
-		# === Examples 
-		#   customer_management_service.get_accounts_info 
+		# +only_parent_accounts+ - boolean to determine whether to return only the accounts that belong to the customer or to also
+		# return the accounts that the customer manages for other customers. Default false
+		#
+		# === Examples
+		#   customer_management_service.get_accounts_info
 		#   # => Array[BingAdsApi::AccountsInfo]
-		# 
+		#
 		# Returns:: Array of BingAdsApi::AccountsInfo
-		# 
+		#
 		# Raises:: exception
 		def get_accounts_info(customer_id=nil, only_parent_accounts=false)
-			response = call(:get_accounts_info, 
-				{customer_id: customer_id || self.client_proxy.customer_id, 
+			response = call(:get_accounts_info,
+				{customer_id: customer_id || self.client_proxy.customer_id,
 				only_parent_accounts: only_parent_accounts.to_s})
 			response_hash = get_response_hash(response, __method__)
 			accounts = response_hash[:accounts_info][:account_info].map do |account_hash|
@@ -84,9 +84,9 @@ module BingAdsApi
 		# Returns:: Array of BingAdsApi::AccountInfoWithCustomerData
 		#
 		# Raises:: exception
-		def find_accounts_or_customers_info(filter='', top_n=1, application_type=BingAdsApi::ApplicationType::ADVERTISER)
+		def find_accounts_or_customers_info(filter='', top_n=1)
 			response = call(:find_accounts_or_customers_info,
-											{filter: filter, top_n: top_n, application_scope: application_type})
+											{filter: filter, top_n: top_n})
 			response_hash = get_response_hash(response, __method__)
 			data = response_hash[:account_info_with_customer_data][:account_info_with_customer_data]
 			if data.is_a? Hash
@@ -102,7 +102,7 @@ module BingAdsApi
 			def get_service_name
 				"customer_management"
 			end
-			
+
 	end
 
 end
